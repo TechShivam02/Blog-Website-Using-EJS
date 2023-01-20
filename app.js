a601f24b8dc061a013db5e1e8b641728d6f59915
TechShivam02 <shivamtech2024@gmail.com>
2023-01-20 23:19:30 +0530

    Blog website using EJS

--- app.js
+++ app.js
@@ -106,10 +106,8 @@ app.post("/compose" , function(req,res){
 
 
 
+const PORT = process.env.PORT || 3030;
 
-
-
-
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
